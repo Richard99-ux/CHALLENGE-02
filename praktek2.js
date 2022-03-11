0afdaaf4e22057caa3051dd552311c9b018d653b
Richard99-ux <kancilcakep99@gmail.com>
2022-03-11 16:47:58 +0700

    feat: perubahan coding praktek2.js

--- praktek2.js
+++ praktek2.js
@@ -1,17 +1,10 @@
 let checkTypeNumber = (givenNumber) => {
-  if (givenNumber != null) {
-    if (typeof(givenNumber) == 'number') {
-        if (givenNumber% 2 == 0) {
-            return "GENAP";
-        } else {
-            return "GANJIL";
-        }
-    } else {
-        return "Error: Invalid data type";
-    }  
-  } else {
-    return "Error: Bro where is the parameter?";
-  }      
+  if (givenNumber != null)
+  if (typeof(givenNumber) == 'number')
+  if (givenNumber % 2 == 0) return "GENAP";
+  else return "GANJIL";
+  else return "Error: Invalid data type";
+  else return "Error: Bro where is the parameter?";
 }
 
 
